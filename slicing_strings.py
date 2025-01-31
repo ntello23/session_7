ab025b65ed1ebbbea691caa9ce44ff515a12a68b
ntelloyoung <ntelloyoung@gmail.com>
2025-01-31 12:26:44 +0100

    Added string slicing

---
+++ slicing_strings.py
@@ -0,0 +1,15 @@
+s = 'abcdefghijklmnopqrstuvwxyz'
+print(s)
+print(s[1:4], s[6:9])
+print(s[:4], s[6:])
+print(s[1:10:2])
+print(s[::-1])
+print('racecar'[::-1])
+
+s = 'cat'
+s = 'r' + s[1:]
+print(s)
+
+s = 'seven'
+s = s[:2] + '7' + s[3:]
+print(s)
